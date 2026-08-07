003244e70f9dde623d4600425d53ad85ce3fd570
Rafael Neves <rafael.neves@ufrn.br>
2026-08-07 16:49:49 -0300

    fix: fix vercel build lint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     devIndicators: false,
+    eslint: {
+        ignoreDuringBuilds: process.env.VERCEL === '1',
+    },
 };
 
 export default nextConfig;
